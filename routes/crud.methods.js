bf8dd099c7ef05649dff397f6f3ead011f13de29
Matías Herrera <matias7122@gmail.com>
2022-11-25 22:20:51 -0300

    main CRUD routes created

--- routes/crud.methods.js
+++ routes/crud.methods.js
@@ -1,10 +1,17 @@
 const { Router } = require('express');
-const { getUsers, addUser } = require('../controllers/crud.methods.js');
+const { GetUsers, GetOneUser, AddUser, UpdateUser, DeleteUser } = require('../controllers/crud.methods.js');
 
 const router = new Router();
 
-router.get('/get-users', getUsers);
+router.get('/get-users', GetUsers);
 
-router.post('/add-user', addUser);
+router.get('/get-user/:id', GetOneUser);
+
+router.post('/add-user', AddUser);
+
+router.put('/update-user/:id', UpdateUser);
+
+router.delete('/delete-user/:id', DeleteUser);
+
 
 module.exports = router;
