a6e7a14ccd8260a3e5c560c0983cb6cd65867c96
Matías Herrera <matias7122@gmail.com>
2022-11-25 22:24:57 -0300

    Connection.js file ready

--- database/connection.js
+++ database/connection.js
@@ -2,8 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const ConnectionToDB = () => {
     const sequelize = new Sequelize('api_database', 'root', '123456789', {
-        host: 'localhost:3306',
-        dialect: 'sqlite'
+        host: 'localhost',
+        dialect: 'mysql'
     })
 
     try {
